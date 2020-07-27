d48719db7ee17a3d916c16c7f1e69acc44e98c2c
wujianhui <wujianhui@f-road.com.cn>
2020-07-27 15:18:51 +0800

    mavon-editor cdn外链改成本地加载

--- panda-manager/src/views/note/data/preview.vue
+++ panda-manager/src/views/note/data/preview.vue
@@ -8,22 +8,43 @@
         :toolbarsFlag = "false"
         :editable="false"
         :scrollStyle="true"
+        :externalLink="externalLink"
         :ishljs = "true"/>
 </template>
 
 <script>
-import { mavonEditor } from 'mavon-editor'
-import 'mavon-editor/dist/css/index.css'
-
 export default {
     name: 'note_data_preview',
-    components: {
-        mavonEditor
-    },
     data() {
         return {
             id: this.$route.params.id,
-            content:''
+            content:'',
+            externalLink: {
+                markdown_css: function() {
+                    // 这是你的markdown css文件路径
+                    return '/markdown/github-markdown.min.css';
+                },
+                hljs_js: function() {
+                    // 这是你的hljs文件路径
+                    return '/highlightjs/highlight.min.js';
+                },
+                hljs_css: function(css) {
+                    // 这是你的代码高亮配色文件路径
+                    return '/highlightjs/styles/' + css + '.min.css';
+                },
+                hljs_lang: function(lang) {
+                    // 这是你的代码高亮语言解析路径
+                    return '/highlightjs/languages/' + lang + '.min.js';
+                },
+                katex_css: function() {
+                    // 这是你的katex配色方案路径路径
+                    return '/katex/katex.min.css';
+                },
+                katex_js: function() {
+                    // 这是你的katex.js路径
+                    return '/katex/katex.min.js';
+                },
+            }
         }
     },
     created () {
